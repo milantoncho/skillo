7e92effc2238029b5c7c88887e2c942829d8cb57
Anton D <avd@mail.bg>
2023-10-18 13:01:34 +0300

    Homework 4 - Problem 3 - fixing a typo

---
+++ Lesson4/H04-Ex04.py
@@ -0,0 +1,5 @@
+# Problem 4: Online Shopping Cart
+# Create a Python program that simulates an online shopping cart using a global dictionary
+# variable. Every customer has unique id as a key. Define functions to add items to the cart,
+# remove items, calculate the total price, and display the contents of the cart. Allow the user to
+# interact with the cart by adding and removing items.
