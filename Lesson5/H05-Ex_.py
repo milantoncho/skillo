535948059e8a42f73aef81b0690391a8f6a206a7
Anton Danchev <avd@mail.bg>
2023-10-21 02:13:32 +0300

    Homework5

---
+++ Lesson5/H05-Ex_.py
@@ -0,0 +1,38 @@
+#TODO: 1. Create a list with the numbers from 1 to 10 and print it.
+def print_list(limit):
+    number_list = list(range(1, limit+1))
+    # print(number_list)
+    # for i in number_list:
+    #     print(i)
+    return number_list
+
+
+print(print_list(11))
+# print_list(11)
+
+#TODO: 1.1. Create a list with the numbers from 1 to 1000 and print it.
+
+# print(print_list(100))
+
+
+#TODO: 2. Reverse the order of the elements in the list from problem 1 and print the result.
+
+# print(print_list(10).reverse())  #Why is this failing?
+rev = print_list(10)
+rev.reverse()
+print(rev)
+
+
+#TODO 3. Given a list of words, create a new list containing the lengths of each word.
+
+family_name = ["Pacino", "Washington", "Penn", "Schwarzenegger", "Oz", "Roth"]
+family_name_len = []
+for i in family_name:
+    family_name_len.append(len(i))
+print(family_name)
+print(family_name_len)
+
+
+
+
+#TODO: 3.1. Given a list of words, create a new dictionary mapping every word to it's length.
