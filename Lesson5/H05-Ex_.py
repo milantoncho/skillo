0bf342e23a118f3141ba8f6a7bcd894053bdc7c8
Anton Danchev <avd@mail.bg>
2023-10-21 10:45:05 +0300

    Homework5

--- Lesson5/H05-Ex_.py
+++ Lesson5/H05-Ex_.py
@@ -26,13 +26,21 @@ print(rev)
 #TODO 3. Given a list of words, create a new list containing the lengths of each word.
 
 family_name = ["Pacino", "Washington", "Penn", "Schwarzenegger", "Oz", "Roth"]
-family_name_len = []
-for i in family_name:
-    family_name_len.append(len(i))
+def list_items_len (some_list):
+    family_name_len = []
+    for names in some_list:
+        family_name_len.append(len(names))
+    return family_name_len
+
 print(family_name)
-print(family_name_len)
-
-
+print(list_items_len(family_name))
 
 
 #TODO: 3.1. Given a list of words, create a new dictionary mapping every word to it's length.
+def list_to_dict(some_list):
+    d = {}
+    for index, name in enumerate(some_list):
+        d[index] = name
+    return d
+
+print(list_to_dict(family_name))
